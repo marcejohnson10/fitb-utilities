0a14c10d3e67100726a32e573568ccd9eae2ed3a
marc <marcejohnson10@yahoo.com>
2023-05-11 19:41:15 -0400

    Update fitb_standard.sql

--- macros/fitb_standard.sql
+++ macros/fitb_standard.sql
@@ -1,7 +1,7 @@
 {% macro generate_schema_name(custom_schema_name, node) -%}
 
 
-    {% set dbt_job_run_id = env_var('DBT_CLOUD_JOB_ID') %}
+   {# {% set dbt_job_run_id = env_var('DBT_CLOUD_JOB_ID') %} #}
     {%- set default_schema = target.schema -%}
 
     {%- if custom_schema_name is none -%}
@@ -9,9 +9,9 @@
         {{ default_schema }}
 
     {%- else -%}
-        {% if  env_var('DBT_ENVIRONMENT') == "sandbox" and dbt_job_run_id is none -%} 
+        {% if  env_var('DBT_ENVIRONMENT') == "sandbo" -%} 
             {{target.user}}_{{ custom_schema_name | trim }}
-        {%- elif env_var('DBT_ENVIRONMENT') == "sandbox" and dbt_job_run_id is not none -%}-%}
+        {%- elif env_var('DBT_ENVIRONMENT') == "sandbox" -%}-%}
             {{default_schema}}_{{ custom_schema_name | trim }}
         {%- else -%}            
             {{ custom_schema_name | trim }}           
