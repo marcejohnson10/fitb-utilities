8e0fcfb8550f6c79ca9541cbf5acebd1078ee39d
marc <marcejohnson10@yahoo.com>
2023-05-19 16:47:14 -0400

    Update fitb_standard.sql

--- macros/fitb_standard.sql
+++ macros/fitb_standard.sql
@@ -1,4 +1,4 @@
-{#
+
 {% macro generate_schema_name(custom_schema_name, node) -%}
     {%- set default_schema = target.schema -%}
     {%- if custom_schema_name is none -%}
@@ -7,11 +7,11 @@
 
     {%- else -%}
 
-        {{ default_schema }}_{{ custom_schema_name | trim }}
+        {{ custom_schema_name | trim }}
 
     {%- endif -%}
 {%- endmacro %}
-#}
+
 {#
 {% macro generate_schema_name(custom_schema_name, node) -%}
     {%- set default_schema = target.schema -%}
